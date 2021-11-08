07528f3540ae9b8ebdab73e2b6499154639390ad
Jung-min Lee <wjdals020503@naver.com>
2021-11-08 20:59:04 +0900

    Modified at 8/11/2021

---
+++ app/src/main/java/com/example/min/MemorizeWords.java
@@ -0,0 +1,14 @@
+package com.example.min;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MemorizeWords extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memorize_words);
+    }
+}
