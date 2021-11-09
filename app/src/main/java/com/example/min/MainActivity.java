d3aa210851dd204df7c38f5365a1aa2bbf79c6d9
seominbi <zyuqw1193@naver.com>
2021-11-09 22:03:52 +0900

    main

--- app/src/main/java/com/example/min/MainActivity.java
+++ app/src/main/java/com/example/min/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.min;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabHost tabHost=findViewById(R.id.host);
+        tabHost.setup();
+
+        TabHost.TabSpec spec= tabHost.newTabSpec("tab1");
+        spec.setIndicator(null, ResourcesCompat.getDrawable(getResources(),R.drawable.mypage_icon,null));
+        spec.setContent(R.id.main1);
+        tabHost.addTab(spec);
+
+
+        spec= tabHost.newTabSpec("tab2");
+        spec.setIndicator(null, ResourcesCompat.getDrawable(getResources(),R.drawable.notice_icon,null));
+        spec.setContent(R.id.main2);
+        tabHost.addTab(spec);
+
+        spec= tabHost.newTabSpec("tab3");
+        spec.setIndicator(null, ResourcesCompat.getDrawable(getResources(),R.drawable.setting_icon,null));
+        spec.setContent(R.id.main3);
+        tabHost.addTab(spec);
     }
 }
