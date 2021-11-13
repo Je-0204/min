f10841f18f1fb832d311a2881b2fef1530335fbb
Jung-min Lee <wjdals020503@naver.com>
2021-11-13 16:00:05 +0900

    Modified at 13/11/2021

---
+++ app/src/main/java/com/example/min/SettingsManageSharedVocabulary.java
@@ -0,0 +1,14 @@
+package com.example.min;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingsManageSharedVocabulary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_manage_shared_vocabulary);
+    }
+}
