e65a7a818754706c6158f55f9036b02f38b78526
Jung-min Lee <wjdals020503@naver.com>
2021-11-21 21:05:26 +0900

    Modified at 11/21

--- app/src/main/java/com/example/min/SettingsModifyUserProfile.java
+++ app/src/main/java/com/example/min/SettingsModifyUserProfile.java
@@ -11,4 +11,6 @@ public class SettingsModifyUserProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_modify_user_profile);
     }
+
+
 }
