be80ce5b91df74b6757a0b27c2a9037a31e54df6
Jung-min Lee <wjdals020503@naver.com>
2021-11-23 12:35:10 +0900

    add permission check

--- app/src/main/java/com/example/min/SettingsModifyUserProfile.java
+++ app/src/main/java/com/example/min/SettingsModifyUserProfile.java
@@ -1,16 +1,82 @@
 package com.example.min;
 
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class SettingsModifyUserProfile extends AppCompatActivity {
+
+public class SettingsModifyUserProfile extends AppCompatActivity{
+    private final int STORAGE_PERMISSION_CODE = 1;
+    ImageView profile_image;
+    Button btn_change_profile_image;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_modify_user_profile);
+
+        Button btn_request = findViewById(R.id.btn_change_profile_image);
+        btn_request.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(ContextCompat.checkSelfPermission(SettingsModifyUserProfile.this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(SettingsModifyUserProfile.this, "You have already granted this permission", Toast.LENGTH_SHORT).show()
+                } else {
+                    requestStoragePermission();
+                }
+            }
+        });
     }
 
+    private void requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission needed")
+                    .setMessage("This permission is needed because fo ths and that")
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(SettingsModifyUserProfile.this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+                        }
+                    })
+                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+        }
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == STORAGE_PERMISSION_CODE) {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Permission GRANTED", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this, "Permission DINIED", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
